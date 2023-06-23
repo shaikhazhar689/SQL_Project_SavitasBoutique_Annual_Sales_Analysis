2be0423ec9ee5fc4e864129ff62bffd5e959b9ad
Azhar Shaikh <shaikhazhar689@gmail.com>
2023-06-24 00:20:59 +0530

    Update

--- project_query.sql
+++ project_query.sql
@@ -1,22 +1,22 @@
 Select * From savitasboutique.supplier;
 
-Select * From savitasboutique.sales;
+Select * From savitasboutique.customer;
 
 Select * From savitasboutique.expenses;
 
 -- Right Join between Two tables supplier and sales
 
-Select supplier.Transaction_id, supplier.Date, sales.Customer_Name, supplier.Quantity, supplier.Tax, 
+Select supplier.Transaction_id, supplier.Date, customer.Customer_Name, supplier.Quantity, supplier.Tax, 
 supplier.Selling_Price, supplier.Cost_Price, supplier.Profit, supplier.Discount, supplier.Supplier_Name
-From savitasboutique.sales
+From savitasboutique.customer
 Right join savitasboutique.supplier
-On supplier.Transaction_id = sales.Transaction_id
+On supplier.Transaction_id = customer.Transaction_id
 Order By supplier.Transaction_id ASC;
 
 -- Top 10 customers by Profit
 
 Select B.Customer_Name, sum(A.Selling_Price) As Total_Profit
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Group By B.Customer_Name
 Order By Total_Profit DESC
@@ -25,7 +25,7 @@ Limit 10;
 -- Top !0 most frequently visiting Customers 
 
 Select count(Distinct A.Transaction_id) As Total, B.Customer_Name
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Group by B.Customer_Name
 Order by Total DESC
@@ -34,49 +34,49 @@ Limit 10;
 -- Top 10 Customers by Quantiy Purchased
 
 Select sum(A.Quantity) As Total_Quantity, B.Customer_Name
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
-Group by Customer_Name
+Group by B.Customer_Name
 Order by Total_Quantity DESC
 Limit 10;
 
 -- Max Profit by customer in single transaction
 
-Select Max(A.Profit) As Max_Profit, B.Customer_Name, A.Transaction_id
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+Select B.Customer_Name, A.Supplier_Name, A.Quantity, Max(A.Profit) As Max_Profit
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
-Group by B.Customer_Name, A.Transaction_id
+Group by B.Customer_Name, A.Supplier_Name, A.Quantity
 Order by Max_Profit DESC
 Limit 10;
 
 -- Total Sales 
 
 Select sum(A.Selling_Price) As Total_Sales
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Total Profit 
 
 Select sum(A.Profit) As Total_Profit
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Total Quantity Sold
 
 Select sum(A.Quantity) As Total_Quantity
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Total Discount
 
 Select sum(A.Discount) As Total_Discount
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Average Discount
 
 Select AVG(A.Discount) As Avg_Discount
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Using CASE Function..
@@ -86,13 +86,13 @@ CASE
  When B.Customer_Name = 'Dr.Pallavi' then 'Dr.Pallavi Dharmadhikari'
  Else B.Customer_Name 
  End As cleaned_name
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id;
 
 -- Customers Total Profit by Profit Percentage
 
 Select B.Customer_Name,  sum(A.Profit) As Total_Profit, sum(A.Profit)/sum(A.Cost_Price)*100 As Profit_Percentage
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Group by B.Customer_Name
 Order by Total_Profit DESC;
@@ -100,7 +100,7 @@ Order by Total_Profit DESC;
 -- Total Profit earned and Quantity purchased by every customer of specific supplier..  
 
 Select  B.Customer_Name, Sum(A.Quantity) As Total_Quantity, Sum(A.Profit) AS Total_Profit 
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Where A.Supplier_Name='Pehnava' or A.Supplier_Name='ADA'
 Group By B.Customer_Name
@@ -110,7 +110,7 @@ Order By Total_Profit DESC;
 -- Quantity Purchased for specific range of cost price..
 
 Select Sum(A.Quantity) As Total_Quantity
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Where A.Cost_Price Between 3000 And 10000;
 
@@ -118,7 +118,7 @@ Where A.Cost_Price Between 3000 And 10000;
 
 Select B.Customer_Name, A.Supplier_Name, A.Selling_Price, A.Cost_Price, A.Discount, A.Profit, 
 A.Profit/A.Cost_Price*100 As Profit_Percentage
-From savitasboutique.supplier As A Left Join Savitasboutique.sales As B 
+From savitasboutique.supplier As A Left Join Savitasboutique.customer As B 
 On A.Transaction_id = B.Transaction_id
 Where A.Cost_Price Between 15000 And 25000
 Order by Profit_Percentage DESC;
